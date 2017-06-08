483956de94c8fd072e84ea26428f8e3423fe445a
Gupitan <bursahelm@gmail.com>
2017-06-08 13:09:21 +0700

    Create autolink.js

--- autolink.js
+++ autolink.js
@@ -1,4 +1,3 @@
-//<![CDATA[
 protected_links = "weblyrics.net,blog.kangismet.net,bungfrangki.com,kompiajaib.com,youtube.com,facebook.com,google.com";
   (function() {
     var sl = document.createElement('script');
@@ -6,4 +5,3 @@ protected_links = "weblyrics.net,blog.kangismet.net,bungfrangki.com,kompiajaib.c
     sl.src = 'https://cdn.rawgit.com/gupitan/safelink-js/dd76c17d/kompisafelink2.js';
     (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(sl);
   })();
-//]]>
