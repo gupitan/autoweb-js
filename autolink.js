aae1e05850826baf71b1d4ceb7eaf1d65fd835c9
Gupitan <bursahelm@gmail.com>
2017-06-08 12:56:14 +0700

    Add files via upload

---
+++ autolink.js
@@ -0,0 +1,9 @@
+//<![CDATA[
+protected_links = "weblyrics.net,blog.kangismet.net,bungfrangki.com,kompiajaib.com,youtube.com,facebook.com,google.com";
+  (function() {
+    var sl = document.createElement('script');
+    sl.type = 'text/javascript'; sl.async = true;
+    sl.src = 'https://cdn.rawgit.com/gupitan/safelink-js/dd76c17d/kompisafelink2.js';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(sl);
+  })();
+//]]>
